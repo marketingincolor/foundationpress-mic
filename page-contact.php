35e26004cf0f4de2460d02bd61893650aa65746d
nbeers22 <nbeers22@gmail.com>
2018-07-26 13:32:57 -0500

    Added top title section for all pages with styles

---
+++ page-contact.php
@@ -0,0 +1,22 @@
+<?php 
+	/* Template Name: Contact */
+	get_header();
+?>
+
+<section class="top-title">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="small-12 cell">
+				<h1>Contact Us</h1>
+			</div>
+			<div class="medium-8 cell">
+				<blockquote class="orange">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum adipisci dolores vitae harum repellat quasi ad corrupti sed enim ipsam rem maiores dolorem at.</p>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php get_footer(); ?>
