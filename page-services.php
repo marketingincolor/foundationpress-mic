1539f40913d610055eaae3531fdadbdfcdad6ace
Edd Twilbeck <etwilbeck@marketingincolor.com>
2018-07-26 16:06:29 -0500

    initial commit of new pages

---
+++ page-services.php
@@ -0,0 +1,147 @@
+<?php 
+	/* Template Name: Services */
+	get_header();
+?>
+
+<section class="page-header relative">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="small-12 cell">
+				<a href="<?php echo site_url(); ?>" class="logo absolute"><img src="<?php bloginfo('template_directory'); ?>/src/assets/images/gray-logo.png" alt="<?php bloginfo('title'); ?>"></a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="top-title">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="small-12 cell">
+				<h1>Our Services</h1>
+			</div>
+			<div class="medium-8 cell">
+				<blockquote class="orange">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum adipisci dolores vitae harum repellat quasi ad corrupti sed enim ipsam rem maiores dolorem at.</p>
+					<a href="#" class="btn-arrow orange">Let's work together <i class="fas fa-arrow-right"></i></a>
+				</blockquote>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="strategize relative">
+	<div class="graphic absolute">
+		<img src="<?php bloginfo('template_directory'); ?>/src/assets/images/strategize.jpg" class="absolute" alt="marketing in color strategize">
+	</div>
+	<div class="grid-container">
+		<div class="grid-x relative">
+			<div class="cell medium-8 gray-box">
+				<div class="grid-x">
+					<div class="cell medium-8">
+						<h3>Strategize</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh. Pellentesque eu dolor vitae lectus hendrerit convallis. Phasellus tincidunt et est sodales gravida. Praesent lacinia commodo luctus. Vestibulum nec dui eros. Cras vel nisl cursus, eleifend enim non, sodales elit. Morbi lectus eros, posuere in auctor vitae, laoreet in nibh.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="plan relative">
+	<div class="graphic absolute">
+		<img src="<?php bloginfo('template_directory'); ?>/src/assets/images/strategize.jpg" class="absolute" alt="marketing in color strategize">
+	</div>
+	<div class="grid-container">
+		<div class="grid-x relative">
+			<div class="cell medium-8 medium-offset-4 gray-box">
+				<div class="grid-x">
+					<div class="cell medium-8 medium-offset-4">
+						<h3>Plan</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh. Pellentesque eu dolor vitae lectus hendrerit convallis. Phasellus tincidunt et est sodales gravida. Praesent lacinia commodo luctus. Vestibulum nec dui eros. Cras vel nisl cursus, eleifend enim non, sodales elit. Morbi lectus eros, posuere in auctor vitae, laoreet in nibh.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="execute relative">
+	<div class="grid-container">
+		<div class="grid-x relative">
+			<div class="cell medium-8">
+				<div class="grid-x">
+					<div class="cell medium-8">
+						<h3>Execute</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+				</div>
+			</div>
+			<div class="cell medium-12">
+				<div class="grid-x">
+					<div class="cell medium-6">
+						<h4>Branding</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+					<div class="cell medium-6">
+						<h4>Email Marketing</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+					<div class="cell medium-6">
+						<h4>Web Design</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+					<div class="cell medium-6">
+						<h4>Video Production</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+					<div class="cell medium-6">
+						<h4>Digital Marketing</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+					<div class="cell medium-6">
+						<h4>Development</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="analyze relative">
+	<div class="graphic absolute">
+		<img src="<?php bloginfo('template_directory'); ?>/src/assets/images/strategize.jpg" class="absolute" alt="marketing in color strategize">
+	</div>
+	<div class="grid-container">
+		<div class="grid-x relative">
+			<div class="cell medium-8 gray-box">
+				<div class="grid-x">
+					<div class="cell medium-8">
+						<h3>Analyze</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh. Pellentesque eu dolor vitae lectus hendrerit convallis. Phasellus tincidunt et est sodales gravida. Praesent lacinia commodo luctus. Vestibulum nec dui eros. Cras vel nisl cursus, eleifend enim non, sodales elit. Morbi lectus eros, posuere in auctor vitae, laoreet in nibh.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="repeat relative">
+	<div class="graphic absolute">
+		<img src="<?php bloginfo('template_directory'); ?>/src/assets/images/strategize.jpg" class="absolute" alt="marketing in color strategize">
+	</div>
+	<div class="grid-container">
+		<div class="grid-x relative">
+			<div class="cell medium-8 medium-offset-4 gray-box">
+				<div class="grid-x">
+					<div class="cell medium-8 medium-offset-4">
+						<h3>Repeat</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tortor erat, facilisis vitae maximus sed, facilisis a nibh. Pellentesque eu dolor vitae lectus hendrerit convallis. Phasellus tincidunt et est sodales gravida. Praesent lacinia commodo luctus. Vestibulum nec dui eros. Cras vel nisl cursus, eleifend enim non, sodales elit. Morbi lectus eros, posuere in auctor vitae, laoreet in nibh.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
